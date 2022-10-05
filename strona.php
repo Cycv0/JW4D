db5d67e9971a9beef01a9a436db4b70c4842865a
Cycv0 <09kuba@gmail.com>
2022-10-05 14:43:37 +0200

    zad1

---
+++ strona.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<p style="background-color: aliceblue; color: darkblue" > <?=abs(-5)?> </p>
+<p style="background-color: aliceblue; color: darkblue" > <?=min(0, 150, 30, 20, -8, -200)?> </p>
+<p style="background-color: aliceblue; color: darkblue" > <?=max(0, 150, 30, 20, -8, -200) ?> </p>
+<p style="background-color: aliceblue; color: darkblue" > <?=pi() ?> </p>
+<p style="background-color: aliceblue; color: darkblue" > <?= round(3.4) ?> </p>
+<p style="background-color: aliceblue; color: darkblue" > <?= round(3.6) ?> </p>
+<p style="background-color: aliceblue; color: darkblue" > <?= round (5.045, 2) ?> </p>
+<p style="background-color: aliceblue; color: darkblue" > <?= round (5.055, 2) ?> </p>
+<p style="background-color: aliceblue; color: darkblue" > <?= rand(900, 1000) ?> </p>
+</body>
+</html>
+<?php
